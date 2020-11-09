4b839b7317eaeee9ed48790d787d63de83db04a9
Poul Henriksen <pohe@easj.dk>
2020-11-09 09:30:51 +0100

    Add project files.

---
+++ HelloWorldRazorPages/Models/Car.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorldRazorPages.Models
+{
+    public class Car
+    {
+        public string Name { get; set; }
+        public int Year { get; set; }
+    }
+}
---
+++ HelloWorldRazorPages/Pages/Index.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HelloWorldRazorPages.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace HelloWorldRazorPages.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public string Message { get; set; }
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public Car Car { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            Car = new Car(){Name = "Toyota", Year = 2020};
+            Message = "Hej fra index page model ";
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            _name = "Velkommen GET requetil Pouls hjemmeside";
+        }
+
+        public void OnPost()
+        {
+            _name = "Velkommen POst request til Pouls hjemmeside";
+        }
+    }
+}
